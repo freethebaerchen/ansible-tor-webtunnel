3fd37709e102490781449715ab27679ed910f794
jochen <jochen@tapesandpips.com>
2024-12-10 23:55:43 +0100

    feat: added plugin for connection string

---
+++ roles/tor/library/get-connection-string.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+from ansible.module_utils.basic import AnsibleModule
+import re
+import random
+
+def get_fingerprint_from_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                if "Your Tor bridge's hashed identity key fingerprint is" in line:
+                    match = re.search(r"[A-F0-9]{40}", line)
+                    if match:
+                        return match.group(0)
+    except FileNotFoundError:
+        print(f"Error: File not found - {file_path}")
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+    return None
+
+def get_url_from_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                if "ServerTransportOptions webtunnel" in line:
+                    match = re.search(r"url=(.*)", line)
+                    if match:
+                        return match.group(0)
+    except FileNotFoundError:
+        print(f"Error: File not found - {file_path}")
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+    return None
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            syslog_path=dict(type='str', required=True),
+            torrc_path=dict(type='str', required=True)
+        )
+    )
+
+    syslog_path = module.params['syslog_path']
+    torrc_path = module.params['torrc_path']
+
+    fingerprint = get_fingerprint_from_file(syslog_path)
+    if not fingerprint:
+        module.fail_json(msg="Fingerprint not found.")
+
+    third_octet = random.randint(2, 253)
+    fourth_octet = random.randint(2, 253)
+
+    url = get_url_from_file(torrc_path)
+    if not url:
+        module.fail_json(msg="URL not found.")
+
+    output = f"webtunnel 10.0.{third_octet}.{fourth_octet}:443 {fingerprint} url={url} ver=0.0.1"
+    module.exit_json(changed=False, stdout=output, url=url, fingerprint=fingerprint)
+
+if __name__ == "__main__":
+    main()

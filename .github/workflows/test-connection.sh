a731179d3cdfd5b64e8099aec2a2ccf42b2e0714
jochen <jochen@tapesandpips.com>
2025-01-18 02:07:19 +0100

    chore: added workflow - WIP

---
+++ .github/workflows/test-connection.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+input_file="$PWD/../../connection-strings.txt"
+
+if [[ ! -f $input_file ]]; then
+  echo "File $input_file does not exist."
+  exit 1
+fi
+
+while IFS= read -r line; do
+  [[ -z "$line" ]] && continue
+
+  node_name="${line##*#}"
+
+  response=$(docker run -e CONNECTION_STRING="$line" bubatzlegal/webtunnel-client:latest)
+
+  if [[ $? -ne 0 ]]; then
+    echo "Docker command failed for node $node_name"
+    continue
+  fi
+
+  if [[ $response == *'"IsTor":false'* ]]; then
+    echo "$node_name: False"
+  elif [[ $response == *'"IsTor":true'* ]]; then
+    echo "$node_name: True"
+  else
+    echo "Something went really wrong for node $node_name"
+  fi
+done < "$input_file"

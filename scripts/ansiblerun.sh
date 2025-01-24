435bddcb44959fc10049a57cd166b21a3b4b7a6b
jke-tapesandpips <jochen@tapesandpips.com>
2025-01-24 03:39:29 +0100

    fix: switch for env file

--- scripts/ansiblerun.sh
+++ scripts/ansiblerun.sh
@@ -15,18 +15,18 @@ if $rebuild || !(docker image ls | grep -q $tag); then
     docker build --no-cache -t $tag -f ansible.Dockerfile .
 fi
 
-if [ -f $(pwd)/.env.local ]; then
-    export $(grep -v '^#' .env.local | xargs)
-else
-    export $(grep -v '^#' .env | xargs)
+env_file=".env.local"
+if [ ! -f $(pwd)/$env_file ]; then
+    env_file=".env"
 fi
+export $(grep -v '^#' $env_file | xargs)
 
 user=$(git config user.email)
 user="${user%@*}"
 user=$(echo "$user" | tr '[:upper:]' '[:lower:]')
 
 if [ "$(uname)" == "Darwin" ]; then
-    docker run -it --rm --network host -v $(pwd):/ansible:rw -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" --env-file .env.local -v ~/.ssh:/tmp/.ssh:ro -e ANSIBLE_REMOTE_USER="${user}" $tag "${args[@]}"    
+    docker run -it --rm --network host -v $(pwd):/ansible:rw -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" --env-file $env_file -v ~/.ssh:/tmp/.ssh:ro -e ANSIBLE_REMOTE_USER="${user}" $tag "${args[@]}"    
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    docker run -it --rm --network host -v $(pwd):/ansible:rw -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} --env-file .env.local -v ~/.ssh:/tmp/.ssh:ro -e ANSIBLE_REMOTE_USER="${user}" $tag "${args[@]}"
+    docker run -it --rm --network host -v $(pwd):/ansible:rw -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} --env-file $env_file -v ~/.ssh:/tmp/.ssh:ro -e ANSIBLE_REMOTE_USER="${user}" $tag "${args[@]}"
 fi
